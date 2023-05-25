91604bc3cb1178dc7e7e4f553933083eb5811c66
Josh Lebedinsky <joshlebed@gmail.com>
2023-05-25 01:33:22 -0400

    add types

--- src/get_spotify_matches.py
+++ src/get_spotify_matches.py
@@ -1,2 +1,13 @@
-def get_spotify_matches(rekordbox_library):
+from rekordbox_library import RekordboxCollection
+
+
+def get_spotify_matches(rekordbox_collection: RekordboxCollection) -> dict[str, str]:
+    """attempt to map all songs in rekordbox library to spotify uris
+
+    Args:
+        rekordbox_collection (RekordboxCollection): set of songs
+
+    Returns:
+        dict[str, str]: map from rekordbox song ID to spotify URI
+    """
     return
