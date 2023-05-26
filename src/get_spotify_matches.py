9ff0c37923f0aa1d46726e739ccbf4950bc4e472
Claudio LoBraico <claudio@lobraico.com>
2023-05-26 00:17:03 -0400

    Add search spotify function

--- src/get_spotify_matches.py
+++ src/get_spotify_matches.py
@@ -1,2 +1,40 @@
-def get_spotify_matches(rekordbox_library):
-    return
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyClientCredentials
+import urllib.parse
+from pprint import pprint
+
+load_dotenv()
+
+
+# def get_spotify_matches(spotify_client: spotipy.Spotify,rekordbox_library):
+#     for track in rekordbox_library:
+#         search_spotify(spotify_client,track=track) # Not supported yet
+#     return
+
+def search_spotify(
+    spotify_client: spotipy.Spotify,
+    filter_map: dict[str,str]=None,
+    keyword_str: str = None,
+    type="track",
+):
+    # TODO: create the filter map using the a rekordbox track type (waiting on josh)
+    filter_list = [f"{key}:{filter_map[key]}" for key in filter_map]
+    if keyword_str is not None:
+        filter_list.insert(0,keyword_str)
+    query = urllib.parse.quote(" ".join(filter_list))
+    return spotify_client.search(q=query,type="track",market="US")
+
+
+def test_search_spotify(artist="Fisher",track="Losing it"):
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    filter_map = {"artist": artist,"track": track}
+    results = search_spotify(spotify,filter_map)
+    tracks = results['tracks']
+    list_of_tracks = results['tracks']['items']
+    for track in list_of_tracks:
+        print(track['name'],track['uri'])
+
+
+if __name__=="__main__":
+    test_search_spotify()
